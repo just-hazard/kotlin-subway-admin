afec7ca8e64d5dd28b1989aeb0d53ff27f6c1c43
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-07-28 18:39:23 +0900

    feat - Sections 개발중

--- src/main/java/nextstep/subway/line/domain/Line.kt
+++ src/main/java/nextstep/subway/line/domain/Line.kt
@@ -24,9 +24,7 @@ class Line(
 
     constructor(name: String, color: String): this(0, name, color, Sections(mutableListOf()))
 
-    companion object {
-        fun of(name: String, color: String, upstation: Station, downStation: Station, distance: Int) : Line {
-            return Line(0,name, color, Sections(this, upstation, downStation, distance))
-        }
+    fun of(name: String, color: String, upStation: Station, downStation: Station, distance: Int) : Line {
+        return Line(0,name, color, Sections(this, upStation, downStation, distance))
     }
 }
