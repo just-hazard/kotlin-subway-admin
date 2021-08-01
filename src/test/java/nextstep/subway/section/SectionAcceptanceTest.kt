3d0790f87e4efed4ca06fc4eca5c808ae31fc02b
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-08-01 22:39:14 +0900

    feat - Section Test file add

---
+++ src/test/java/nextstep/subway/section/SectionAcceptanceTest.kt
@@ -0,0 +1,8 @@
+package nextstep.subway.section
+
+import nextstep.subway.AcceptanceTest
+import org.junit.jupiter.api.DisplayName
+
+@DisplayName("지하철 구간 관련 기능")
+class SectionAcceptanceTest : AcceptanceTest() {
+}
